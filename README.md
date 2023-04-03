8f84a3f132e27119efedcd7a08fa3fdfd97c4d20
sakurawaifu <2606843065@qq.com>
2023-04-03 16:01:37 +0800

    release v1.1.0

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # async-pool-slim
 
-并发执行异步任务，可指定并发上限
+并发执行异步任务，可指定并发上限。
 
-## asyncPool(tasks: Task[], limit: number)
+## 安装
 
-* tasks：Task数组。
-* limit：并发数上限。
+```shell
+pnpm add async-pool-slim
+```
 
-Task 的声明为 ```() => Promise<any>```。返回的 Promise 最终应转为 fulfilled 态，其他逻辑请自行在中间处理。
-
-## example
+## 使用
 
 ```ts
 const task1 = () =>
@@ -22,5 +21,21 @@ const task2 = () =>
     .then(v => console.log(v))
     .catch(e => console.error(e))
 
-asyncPool([task1, task2], 1)
+const task3 = () =>
+  request(url3)
+    .then(v => console.log(v))
+    .catch(e => console.error(e))
+
+asyncPool([task1, task2], 2)
 ```
+
+## API
+
+### asyncPool(tasks: Task[], limit: number): Promise\<undefined>
+
+* tasks：Task 数组。
+* limit：并发数上限。
+
+并发执行异步任务，可指定并发上限。返回一个Promise，所有任务执行完毕后转为 fulfilled 态，value 为undefined。
+
+Task 的声明为 ```() => Promise<any>```。**返回的 Promise 最终应转为 fulfilled 态，其他逻辑请自行在中间处理。**
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 export type Task = () => Promise<any>
 
-const asyncPool = (tasks: Task[], limit: number) => {
-  let nextIndex = 0
-  const next = () => void tasks[nextIndex++]().then(next)
+const asyncPool = (tasks: Task[], limit: number): Promise<undefined> =>
+  new Promise(resolve => {
+    let nextIndex = 0
+    let fulfilledNum = 0
+    const next = () => {
+      if (nextIndex < tasks.length) {
+        tasks[nextIndex++]()
+          .then(() => {
+            fulfilledNum++
+            if (fulfilledNum === tasks.length) {
+              resolve(undefined)
+              return
+            }
 
-  const endIndex = Math.min(limit, tasks.length)
-  for (let i = 0; i < endIndex; i++) {
-    next()
-  }
-}
+            next()
+          })
+      }
+    }
+
+    const endIndex = Math.min(limit, tasks.length)
+    for (let i = 0; i < endIndex; i++) {
+      next()
+    }
+  })
 
 export default asyncPool
